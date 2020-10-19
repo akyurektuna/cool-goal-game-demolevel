c52e3cb9cfe8c753fcd492a4d637867f26b91e67
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2020-10-20 01:47:25 +0300

    initial commit
    adding scenes, models and basic scripts

---
+++ Assets/Scriptler/Point.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Point : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "kale"){
+            Debug.Log("The player has collided with the wall!");
+            SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+        //eger engele carptiysa ve hızı 0 olduysa scene tekrar calistir
+        if(collision.gameObject.tag=="obstacle"){
+            Debug.Log("engele carpti");
+            if(rb.velocity.y==0){
+                SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    
+    }
+}
